a88bd10c7595c6eed052aa1a037382b60a71e5a6
Taha Jadid <jadid.taha@waroom.com>
2025-05-19 21:36:43 +0100

    update packages & add submit design

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,17 +1,36 @@
 import BackButton from '@/components/BackButton';
+import Button from '@/components/Button';
 import Input from '@/components/Input';
 import ScreenWrapper from '@/components/ScreenWrapper';
 import Typo from '@/components/Typo';
 import { colors, spacingY } from '@/constants/theme';
 import { verticalScale } from '@/utils/styling';
+import { useRouter } from 'expo-router';
 import * as Icons from 'phosphor-react-native';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Alert, Pressable, StyleSheet, View } from 'react-native';
 
 const login = () => {
+
+    const emailRef = useRef("");
+    const passswordRef = useRef("");
+    const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async () => {
+        if(!password || !emailRef.current){
+            Alert.alert("Login","Please fill the fields !")
+            return;
+        }
+        console.log("email : ",emailRef.current)
+        console.log("password : ",password)
+    };
+
   return (
     <ScreenWrapper>
         <View style={styles.container}>
+
             <BackButton/>
 
             {/** Top label */}
@@ -31,9 +50,11 @@ const login = () => {
                     Login now to track every expenses
                 </Typo>
 
+
                 {/** email adress Input */}
                 <Input
-                    placeholder='Enter your email..' 
+                    placeholder='Enter your email'
+                    onChangeText={(value) => (emailRef.current = value)}
                     icon={
                         <Icons.At
                             size={verticalScale(26)}
@@ -41,22 +62,53 @@ const login = () => {
                             weight='fill'
                         />
                     }
-                    />
+                />
 
                 {/** password Input */}
                 <Input
-                    placeholder='****' 
+                    placeholder='Enter your password'
+                    secureTextEntry={true} // 🔒 This hides the input
+                    value={password}
+                    onChangeText={setPassword}
                     icon={
-                        <Icons.Eye
+                        <Icons.Lock
                             size={verticalScale(26)}
                             color={colors.neutral300}
                             weight='fill'
                         />
                     }
-                    />
-            
+                />
+
+                <Pressable>
+                    <Typo style={styles.forgetPassword}>
+                        Forget Password ?
+                    </Typo>
+                </Pressable>
+
+                <Button loading={isLoading} onPress={handleSubmit}>
+                    <Typo style={styles.submitButton}>
+                        Login
+                    </Typo>
+                </Button>
+
             </View>
+
+            {/** footer */}
+            <View style={styles.footer}>
+                <Typo style={styles.footerText}>
+                    Don't have an account?
+                </Typo>
+                <Pressable onPress={() => router.push("/(auth)/register")}>
+                    <Typo style={styles.underlinedText}>
+                        Sign up
+                    </Typo>
+                </Pressable>
+            </View>
+
+
         </View>
+        
+
     </ScreenWrapper>
   )
 }
@@ -77,6 +129,11 @@ const styles = StyleSheet.create({
     form: {
         gap: spacingY._20
     },
+    forgetPassword:{
+        fontSize: 14,
+        color: colors.text,
+        alignSelf:"flex-end",
+    },
     footer: { 
         flexDirection: "row",
         justifyContent: "center",
@@ -86,6 +143,17 @@ const styles = StyleSheet.create({
     footerText: {
         textAlign:"center",
         color: colors.text,
-        fontSize: verticalScale(10)
-    }
+        fontSize: 14
+    },
+    underlinedText: {
+        textDecorationLine: 'underline',
+        fontSize: 14,
+        color: colors.primary,
+        fontWeight: "700"
+      },
+    submitButton: {
+        fontSize: 20,
+        color: colors.black,
+        fontWeight: "700"
+    },
 });
