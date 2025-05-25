84dc7fdf5974eb590d2fad953080046b83c0d97d
Taha Jadid <jadid.taha@waroom.com>
2025-05-25 19:37:15 +0100

    Ad login & logout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,22 @@
+import Button from '@/components/Button';
+import Typo from '@/components/Typo';
+import { auth } from '@/config/firebase';
+import { colors } from '@/constants/theme';
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const Home = () => {
+    const handleLogout = async () => {
+        await signOut(auth);
+    };
+    
   return (
     <View>
       <Text>index</Text>
+      <Button onPress={handleLogout}>
+        <Typo color={colors.black}>Logout</Typo>
+      </Button>
     </View>
   )
 }
---
+++ components/CustomTabs.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const CustomTabs = () => {
+  return (
+    <View>
+      <Text>CustomTabs</Text>
+    </View>
+  )
+}
+
+export default CustomTabs
+
+const styles = StyleSheet.create({})
