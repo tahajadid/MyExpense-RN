b928b4730aba2b70c5c5a7f57e3f9d916165f1ae
Taha Jadid <jadid.taha@waroom.com>
2025-05-28 22:06:17 +0100

    Add profuile syyleSheet

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,11 +1,12 @@
+import ScreenWrapper from '@/components/ScreenWrapper';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
 const Statistics = () => {
   return (
-    <View>
+    <ScreenWrapper>
       <Text>statistics</Text>
-    </View>
+    </ScreenWrapper>
   )
 }
 
