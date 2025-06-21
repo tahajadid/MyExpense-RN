02796e2cd71ee6aae814a6c4560d28e9346e53ca
Taha Jadid <jadid.taha@waroom.com>
2025-06-21 23:15:49 +0100

    Add axios library & upload images in cloudinary

--- services/userService.ts
+++ services/userService.ts
@@ -1,12 +1,27 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, UserDataType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFileToCloudinary } from "./imageService";
 
 export const updateUser = async (
     uid: string,
     updatedData: UserDataType
 ): Promise<ResponseType> => {
     try{
+        if(updatedData.image && updatedData?.image?.uri){
+            const imageUploadResponse = await uploadFileToCloudinary(
+                updatedData.image, "users"
+            );
+            if(!imageUploadResponse.success){
+                return {
+                    success:false,
+                    msg: imageUploadResponse.msg || "Failed to upload profile image"
+                }
+            }
+
+            updatedData.image = imageUploadResponse.data;
+        }
+
         const userRef = doc(firestore, "users", uid);
         await updateDoc(userRef, updatedData);
 
