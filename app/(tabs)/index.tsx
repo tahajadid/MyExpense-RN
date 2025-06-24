0b5e30bc23b8abdb3632285d8929f1af0a1e85d8
Taha Jadid <jadid.taha@waroom.com>
2025-06-24 22:09:15 +0100

    Add header of hOME SCREEN

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,82 @@
-import Button from '@/components/Button';
+import HomeCard from '@/components/HomeCard';
 import ScreenWrapper from '@/components/ScreenWrapper';
 import Typo from '@/components/Typo';
-import { auth } from '@/config/firebase';
-import { colors } from '@/constants/theme';
-import { signOut } from 'firebase/auth';
+import { colors, spacingX, spacingY } from '@/constants/theme';
+import { useAuth } from '@/contexts/authContext';
+import { verticalScale } from '@/utils/styling';
+import * as Icons from 'phosphor-react-native';
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native';
 
 const Home = () => {
-    const handleLogout = async () => {
-        await signOut(auth);
-    };
-    
+
+  const {user} = useAuth();
+
   return (
     <ScreenWrapper>
-      <Text>index</Text>
-      <Button onPress={handleLogout}>
-        <Typo color={colors.black}>Logout</Typo>
-      </Button>
+      <View style={styles.container}>
+          {/** Header */}
+          <View style={styles.header}>
+            <View style={{ gap: 4}}>
+            <Typo size={16} color={colors.neutral400}>
+              Hello,
+            </Typo>
+            <Typo size={20} fontWeight={"500"}>{user?.name}</Typo>
+            </View>
+          <TouchableOpacity style={styles.searchIcon}>
+            <Icons.MagnifyingGlass 
+              size={verticalScale(22)}
+              color={colors.neutral200}
+              weight='bold'
+            />
+          </TouchableOpacity>
+          </View>
+      </View>
+
+      {/** Scroll view */}
+      <ScrollView 
+        contentContainerStyle={styles.scrollViewStyle}
+        showsVerticalScrollIndicator={false}
+      >
+        {/** Cards */}
+        <View>
+            <HomeCard />
+        </View>
+      </ScrollView>
     </ScreenWrapper>
   )
 }
 
 export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingX._20,
+    marginTop: verticalScale(8)
+  },
+  header :{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: spacingY._10
+  },
+  searchIcon: {
+    backgroundColor: colors.neutral700,
+    padding: spacingX._10,
+    borderRadius: 50
+  },
+  floatingButton: {
+    height: verticalScale(50),
+    width: verticalScale(50),
+    borderRadius: 100,
+    position: "absolute",
+    bottom: verticalScale(30),
+    right: verticalScale(30)
+  },
+  scrollViewStyle: {
+    marginTop: spacingY._10,
+    paddingBottom: verticalScale(100),
+    gap: spacingY._25
+  }
+})
---
+++ components/HomeCard.tsx
@@ -0,0 +1,74 @@
+import { colors, spacingX, spacingY } from '@/constants/theme'
+import { scale, verticalScale } from '@/utils/styling'
+import { ImageBackground } from 'expo-image'
+import * as Icons from 'phosphor-react-native'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import Typo from './Typo'
+
+const HomeCard = () => {
+  return (
+    <ImageBackground 
+        source={require("../assets/images/card.png")}
+        resizeMode='stretch'
+        style={styles.bgImage}>
+        <View style={styles.container}>
+            <View>
+                {/** balance view */}
+                <View style={styles.totalBalanceRow}>
+                    <Typo color={colors.neutral800}
+                        size={17} fontWeight={"500"}>
+                            Total Balance
+                    </Typo>
+                    <Icons.DotsThreeOutline
+                    size={verticalScale(23)}
+                    color={colors.black}
+                    weight='fill' />
+                </View>
+                <Typo 
+                color={colors.black}
+                size={30} fontWeight={"bold"}>
+                    $522.43
+                </Typo>
+            </View>
+        </View>
+    </ImageBackground>
+  )
+}
+
+export default HomeCard
+
+const styles = StyleSheet.create({
+    bgImage: {
+        height: scale(210),
+        width: "100%"
+    },
+    container: {
+        padding: spacingX._20,
+        paddingHorizontal: scale(23),
+        height: "87%",
+        width: "100%",
+        justifyContent: "space-between",
+    },
+    totalBalanceRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: spacingY._5
+    },
+    stats: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    statsIcon: {
+        backgroundColor: colors.neutral350,
+        padding: spacingY._5,
+        borderRadius: 50
+    },
+    incomeExpense: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: spacingY._7
+    }
+})
